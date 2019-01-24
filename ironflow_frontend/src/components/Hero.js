376361636d7ea0c6071bca8ddc3038fe5b4e0af1
Serpentz <abyicherinet@gmail.com>
2019-01-24 09:27:35 -0500

    front

--- ironflow_frontend/src/components/Hero.js
+++ ironflow_frontend/src/components/Hero.js
@@ -8,8 +8,93 @@ class Hero extends Component {
   render() {
     return (
       <div className="cont">
-            
-      </div>
+        	<div className="hold-illustration w-preserve-3d">
+        		<div className="cs__parent w-preserve-3d">
+        			<div className="cs__hero-section">
+        				<div className="cs__top-bar w-clearfix">
+        					<div className="green-button" data-ix="float-in-top-1" style={{opacity: "1", transform: "translateX(0px) translateY(0px) translateZ(0px)", transition: "opacity 1500ms ease 0s, transform 1500ms ease 0s"}}></div>
+        					<div className="green-button small" data-ix="float-in-top-4" style={{opacity: "1", transform: "translateX(0px) translateY(0px) translateZ(0px)", transition: "opacity 1500ms ease 0s, transform 1500ms ease 0s"}}></div>
+        					<div className="green-button small" data-ix="float-in-top-3" style={{opacity: "1", transform: "translateX(0px) translateY(0px) translateZ(0px)", transition: "opacity 1500ms ease 0s, transform 1500ms ease 0s"}}></div>
+        					<div className="green-button small" data-ix="float-in-top-2" style={{opacity: "1", transform: "translateX(0px) translateY(0px) translateZ(0px)", transition: "opacity 1500ms ease 0s, transform 1500ms ease 0s"}}></div>
+        				</div>
+        				<div className="cs__hero-text w-clearfix">
+        					<div className="hold-letter" data-ix="show-letter-1" style={{opacity: "1", display: "inline-block", transition: "opacity 350ms ease 0s"}}>
+        						<div>I</div>
+        					</div>
+        					<div className="hold-letter" data-ix="show-letter-2" style={{opacity: "1", display: "inline-block", transition: "opacity 350ms ease 0s"}}>
+        						<div>R</div>
+        					</div>
+        					<div className="hold-letter" data-ix="show-letter-3" style={{opacity: "1", display: "inline-block", transition: "opacity 350ms ease 0s"}}>
+        						<div>O</div>
+        					</div>
+        					<div className="hold-letter" data-ix="show-letter-4" style={{opacity: "1", display: "inline-block", transition: "opacity 350ms ease 0s"}}>
+        						<div>N</div>
+        					</div>
+        					<div className="hold-letter" data-ix="show-letter-5" style={{opacity: "1", display: "inline-block", transition: "opacity 350ms ease 0s"}}>
+        						<div>F</div>
+        					</div>
+        					<div className="hold-letter" data-ix="show-letter-7" style={{opacity: "1", display: "inline-block", transition: "opacity 350ms ease 0s"}}>
+        						<div>L</div>
+        					</div>
+        					<div className="hold-letter" data-ix="show-letter-8" style={{opacity: "1", display: "inline-block", transition: "opacity 350ms ease 0s"}}>
+        						<div>O</div>
+        					</div>
+        					<div className="hold-letter" data-ix="show-letter-9" style={{opacity: "1", display: "inline-block", transition: "opacity 350ms ease 0s"}}>
+        						<div>W</div>
+        					</div>
+        					
+        					<div className="hold-letter" data-ix="show-letter-11" style={{opacity: "1", display: "none", transition: "opacity 350ms ease 0s"}}>
+        						<div>d</div>
+        					</div>
+        					<div className="cs__cursor-text-icon" data-ix="cursor-flash" style={{transition: "opacity 350ms linear 0s", opacity: "0.6"}}></div>
+        				</div>
+        				<div className="cs__left-side" data-ix="float-out-from-left" style={{opacity: "1", transform: "translateX(0px) translateY(0px) translateZ(0px)", transition: "opacity 2500ms ease 0s, transform 2500ms ease 0s"}}></div>
+        				<div className="cs__right-block w-preserve-3d" data-ix="float-out-from-right" style={{opacity: "1", transform: "translateX(0px) translateY(0px) translateZ(0px)", transition: "opacity 2500ms ease 0s, transform 2500ms ease 0s"}}></div>
+        			</div>
+        			<div className="cs__content w-clearfix">
+        				<div className="text-like-box">
+        					<div className="overflow-hide w-clearfix">
+        						<div className="text" data-ix="arrow-load" style={{opacity: 1, transition: "opacity 850ms ease 0s;"}}>A</div>
+        						<div className="straight-line" data-ix="arrow-load" style={{opacity: 1, transition: "opacity 850ms ease 0s;"}}></div>
+        						<div className="straight-line-full" data-ix="loading-line-animation" style={{transform: "translateX(0px) translateY(0px) translateZ(0px); transition: transform 1500ms ease 0s;"}}></div>
+        						<div className="straight-line-full" data-ix="loading-line-animation-2" style={{transform: "translateX(0px) translateY(0px) translateZ(0px); transition: transform 1500ms ease 0s;"}}></div>
+        						<div className="straight-line-full" data-ix="loading-line-animation-4-shortened" style={{transform: "translateX(0px) translateY(0px) translateZ(0px); transition: transform 1500ms ease 0s;"}}></div>
+        						<div className="straight-line-full" data-ix="loading-line-animation-5" style={{transform: "translateX(0px) translateY(0px) translateZ(0px); transition: transform 1500ms ease 0s;"}}></div>
+        						<div className="shortened straight-line-full" data-ix="loading-line-animation-6shorted" style={{width: "50%", transition: "width 1500ms ease 0s"}}></div>
+        					</div>
+        				</div>
+        				<div className="cs__hold-image">
+        					<div className="mountain-photo__real-deal w-preserve-3d" data-ix="arrow-load" style={{transition: "transform 0.45s ease 0s, box-shadow 0.45s ease 0s, -webkit-transform 0.45s ease 0s, opacity 850ms ease 0s", opacity: "1"}}>
+        						<div className="mountain" data-ix="mountain-slide-in-from-left" style={{transform: "translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(45deg); transition: transform 2500ms ease 0s"}}></div>
+        						<div className="_2 mountain" data-ix="mountain-slide-in" style={{transform: "translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(45deg)", transition: "transform 2500ms ease 0s"}}></div>
+        					</div>
+        				</div>
+        				<div className="cs__bottom-block"></div>
+        			</div>
+        		</div>
+        		<div className="keyboard-parent w-clearfix">
+        			<div className="keyboard__button" data-ix="type-letter-7" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "1"}}></div>
+        			<div className="_2 keyboard__button" data-ix="type-letter-2" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.9"}}></div>
+        			<div className="_3 keyboard__button" data-ix="type-letter-9" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.8;"}}></div>
+        			<div className="_4 keyboard__button" data-ix="type-letter-8" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.8;"}}></div>
+        			<div className="_5 keyboard__button" data-ix="type-letter-5" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_7 keyboard__button" data-ix="type-letter-3" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_8 keyboard__button" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_9 keyboard__button" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_10 keyboard__button" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_11 keyboard__button" data-ix="type-letter-10" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.6;"}}></div>
+        			<div className="_12 keyboard__button" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_13 keyboard__button" data-ix="type-letter-1" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.5;"}}></div>
+        			<div className="_14 keyboard__button" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_15 keyboard__button" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_16 keyboard__button" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_17 keyboard__button" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="keyboard__button space-bar" data-ix="type-letter-6" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_18 keyboard__button" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        			<div className="_18 keyboard__button" style={{transition: "opacity 0.35s ease 0s, opacity 350ms ease 0s",opacity: "0.75;"}}></div>
+        		</div>
+        	</div>
+        </div>
 
     );
   }
