5e6e0b84eb64ae79e3cadc1691e9681aad92eaed
AB <absalomcherinet@gmail.com>
2021-09-02 14:00:23 -0400

    [INTERNAL] Adding more comments for complex logic

--- ironflow_backend/config/environments/development.rb
+++ ironflow_backend/config/environments/development.rb
@@ -14,6 +14,12 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
+
+    # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
